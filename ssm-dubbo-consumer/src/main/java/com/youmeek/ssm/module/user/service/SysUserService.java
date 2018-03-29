6ba3ff394bb5dcff28d8311dd21fcfd63301dc0f
wukong12306 <wukong12306@sina.com>
2018-03-29 13:35:17 +0800

    delete

--- ssm-dubbo-consumer/src/main/java/com/youmeek/ssm/module/user/service/SysUserService.java
+++
@@ -1,9 +0,0 @@
-package com.youmeek.ssm.module.user.service;
-
-import com.youmeek.ssm.module.user.pojo.SysUser;
-
-public interface SysUserService {
-	SysUser getById(Long id);
-	String get(String id);
-
-}
